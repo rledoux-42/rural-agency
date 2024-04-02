7e7e122a58b638a0dec47f9d04d49e2585d57c0a
rledoux <raphael.ledoux252@gmail.com>
2024-04-02 09:38:04 +0200

    fix(Speed): :rocket: Fix SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="fr">
       <body className={inter.className}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
